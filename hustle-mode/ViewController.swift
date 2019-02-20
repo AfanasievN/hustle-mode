ef3918c2f43b1336691af37427dfdbd28ad3b93c
Nikita Afanasiev <thehikita@gmail.com>
2019-02-20 15:12:01 +0200

    Initial Commit

---
+++ hustle-mode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hustle-mode
+//
+//  Created by Nichita Afanasiev on 2/20/19.
+//  Copyright © 2019 CM-FRUCT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
